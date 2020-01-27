c52c81f275dc132f8ad7927037d7a9066d8fcd32
Ruihong Zhu <raymondzhu1996@gmail.com>
2020-01-27 17:26:27 -0500

    modify museum.py

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,19 @@
-def draw_road():
-    print("road not found")
+def draw_road(length=10, axis="horizontal"):
+    # TODO: implement some variable checks
+    VALID = ["horizontal", "vertical"]
+    if axis not in VALID:
+        # raise error
+        raise ValueError("your axis is undefined")
+    if axis == "horizontal":
+        return draw_road_horizontal(length)
+    else:
+        for i in range(length):
+            print("||")
+    print()
+    return
+
+
+def draw_road_horizontal(length):
+    for i in range(length):
+        print("=", end="")
     return
--- 01-python/leisure/museum.py
+++ 01-python/leisure/museum.py
@@ -1,3 +1,8 @@
-def draw_museum():
-    print("museum not found")
+def draw_museum(length):
+    for i in range(length):
+        print("========")
+        print("historical item" + str(i+1))
+    print("========")
     return
+
+draw_museum(5)
